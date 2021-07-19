f7f9eaa654a736e9947fbba37981b22e32c5d695
Jacky <newbdez33@gmail.com>
2021-07-19 11:54:50 +0900

    fix distribute number precision

--- src/App.js
+++ src/App.js
@@ -19,8 +19,27 @@ function downloadAddresses(pairs) {
   downloadObjectAsJson(addresses, "addresses")
 }
 
+function downloadForSender(pairs, total) {
+  let addresses = "";
+  let each = Math.floor((total / pairs.length) * 100000000)/100000000;
+  for (let index = 0; index < pairs.length; index++) {
+    const p = pairs[index];
+    addresses += `${p.address},${each}\n`;
+  }
+  downloadObjectAsText(addresses, "bulksender")
+}
+
+function downloadObjectAsText(exportObj, exportName){
+  var dataStr = "data:text/plain;charset=utf-8," + encodeURIComponent(exportObj);
+  download(dataStr, exportName);
+}
+
 function downloadObjectAsJson(exportObj, exportName){
   var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 2));
+  download(dataStr, exportName);
+}
+
+function download(dataStr, exportName) {
   var downloadAnchorNode = document.createElement('a');
   downloadAnchorNode.setAttribute("href",     dataStr);
   downloadAnchorNode.setAttribute("download", exportName + ".json");
@@ -31,6 +50,7 @@ function downloadObjectAsJson(exportObj, exportName){
 
 function App() {
   const [n, setN] = useState(10);
+  const [total, setTotal] = useState(0);
   const [pairs, setPairs] = useState([]);
   return (
     <div className="App">
@@ -39,9 +59,17 @@ function App() {
       <div><button onClick={ () => generate(n, setPairs) }>Generate</button> <input value={n} onChange={ (e) => setN(e.target.value) } /> <button onClick={() => { setPairs([])}} >Clear</button></div>
       { 
         pairs.length > 0 ? 
-        <div style={{"marginTop":"20px"}}><button onClick={()=>{downloadPairs(pairs)}} className="link">Download { pairs.length } Key pairs</button> 
-        <span style={{"margin":"10px"}}>|</span>
-        <button onClick={()=>{downloadAddresses(pairs)}} className="link">Download { pairs.length } addresses</button></div> : null 
+        <div style={{"marginTop":"20px"}}>
+          <button onClick={()=>{downloadPairs(pairs)}} className="link">Download { pairs.length } Key pairs</button> 
+          <span style={{"margin":"10px"}}>|</span>
+          <button onClick={()=>{downloadAddresses(pairs)}} className="link">Download { pairs.length } addresses</button>
+          <div style={{"marginTop":"20px"}}>
+            Distribute Total <input value={total} onChange={ (e) => setTotal(e.target.value) } /> Tokens 
+            &nbsp; → &nbsp;
+            <button onClick={()=>{downloadForSender(pairs, total)}} className="link">Download TXT file for Token BulkSender</button>
+          </div>
+        </div> 
+        : null 
       }
       
       <hr />
